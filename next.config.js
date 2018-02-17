d45a57bea90931dca2dcbee9d331f65d6f3aca70
tridung <tridung141196@gmail.com>
2018-02-18 00:13:47 +0700

    change index.html

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  exportPathMap: function() {
-    return {
-      "/": { page: "/" }
-    };
-  },
-  assetPrefix: process.env.NODE_ENV === "production" ? "/waiting-m2love" : ""
-};
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export const getProbability = percents => {
-  return Math.floor(Math.random() * 1000) + 1 < percents * 10;
-};
-
-export const getRandInterval = (min, max) => {
-  return Math.random() * (max - min) + min;
-};
